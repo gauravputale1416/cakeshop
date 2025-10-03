a4af11ef4e02e7df140e0d658550319d15c5a07b
gauravputale1416 <gauravputale2003@gmail.com>
2025-10-04 01:35:08 +0530

    added the new component

--- src/Views/Cake-view.jsx
+++ src/Views/Cake-view.jsx
@@ -3,7 +3,7 @@ import img1 from '../assets/1.jpeg'
 import img2 from '../assets/2.webp'
 import img3 from '../assets/3.jpg'
 import img4 from '../assets/4.png'
-import { CAKEDATA } from '../configes/cakedata.jsx'
+import { CAKEDATA } from '../configes/Cakedata.jsx;'
 
 
 const images = [
